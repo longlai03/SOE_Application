eccabf30533c314e8f83e3f35122661326d3f374
Lamhuong09 <nguyenhuonglam2109@gmail.com>
2024-04-08 21:17:27 +0700

    lần2

--- app/src/main/java/com/example/soeapplication/fragment/UserFragment.java
+++ app/src/main/java/com/example/soeapplication/fragment/UserFragment.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import androidx.fragment.app.Fragment;
 
 import com.example.soeapplication.R;
+import com.example.soeapplication.activity.Account;
+import com.example.soeapplication.activity.Login;
 import com.example.soeapplication.activity.Myprofile;
 
 public class UserFragment extends Fragment {
@@ -23,19 +25,41 @@ public class UserFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
 
-        // Find the button by its ID
         Button infoButton = view.findViewById(R.id.button_info);
+        Button accountButton = view.findViewById(R.id.button_setting);
 
-        // Set click listener for the button
+
         infoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an Intent to start Myprofile activity
+                // Tạo Intent để mở Myprofile activity
                 Intent intent = new Intent(getActivity(), Myprofile.class);
                 startActivity(intent);
             }
         });
 
+
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tạo Intent để mở Account activity
+                Intent intent = new Intent(getActivity(), Account.class);
+                startActivity(intent);
+            }
+        });
+
+        Button logoutButton = view.findViewById(R.id.button_info);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
         return view;
     }
+
 }
