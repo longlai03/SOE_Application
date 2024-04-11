d7c550f1db56108ee93a96eaee175c7b278abfb5
longlai03 <hoanglong2dtd@gmail.com>
2024-04-11 20:14:01 +0700

    changing

--- app/src/main/java/com/example/soeapplication/adapter/CartAdapter.java
+++ app/src/main/java/com/example/soeapplication/adapter/CartAdapter.java
@@ -14,6 +14,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.soeapplication.HelperClass.CartProductClass;
 import com.example.soeapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -29,7 +33,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.item_buy_product, parent, false);
+        View v = LayoutInflater.from(context).inflate(R.layout.item_cart_product, parent, false);
         return new MyViewHolder(v);
     }
 
@@ -45,9 +49,40 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> 
                 holder.product_seller.setText(username);
             }
         });
+        holder.tvplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int quantity = Integer.parseInt(holder.amount.getText().toString());
+                if(quantity <= 99) {
+                    quantity++;
+                    holder.amount.setText(String.valueOf(quantity));
+                    UpdateProductQuantity(cartProduct.getProduct_id(), quantity);
+                }
+            }
+        });
+        holder.tvminus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int quantity = Integer.parseInt(holder.amount.getText().toString());
+                if (quantity > 1) {
+                    quantity--;
+                    holder.amount.setText(String.valueOf(quantity));
+                    UpdateProductQuantity(cartProduct.getProduct_id(), quantity);
+                }
+            }
+        });
         holder.product_cost.setText(cartProduct.getCost());
         holder.date_of_order.setText(cartProduct.getDate_of_order());
         holder.amount.setText(cartProduct.getProduct_quantity());
+    }
+    private void UpdateProductQuantity(String product_id, int quantity){
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        FirebaseDatabase cart_database = FirebaseDatabase.getInstance();
+        DatabaseReference cart_databaseReference = cart_database.getReference("cart");
+        if(mUser != null) {
+            cart_databaseReference.child(mUser.getUid()).child(product_id).child("product_quantity").setValue(String.valueOf(quantity));
+        }
     }
 
     @Override
